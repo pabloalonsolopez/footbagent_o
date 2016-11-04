46ad45dc6c3547b8666fce2d1fe6ce31d638fdf7
PABLO ALONSO LOPEZ <0007988@insags.com>
2016-11-04 15:30:59 +0100

    staff

--- client/app/shared/pipes/f.pipe.ts
+++ client/app/shared/pipes/f.pipe.ts
@@ -9,12 +9,16 @@ export class FPipe implements PipeTransform {
   transform(value: string): string {
     switch(value) {
       case 'form-control':
-        return 'w(100%) b(0) bb(1) bb-focus(brand) py(0.5rem) lh(1.5) ff(proxima) c(black) c-placeholder(gray3) c-focus-placeholder(gray4) ou(0) t(all) t-placeholder(all)'
+        return 'w(100%) b(2) br(2px) b-focus(brand) px(1.5rem) py(0.5rem) lh(1.5) ff(inherit) c(black) c-placeholder(gray4) c-focus-placeholder(gray5) ou(0) t(all) t-placeholder(all)'
       case 'button':
-        return 'b(0) br(2px) px(1.5rem) py(0.5rem) bg(brand) bg-hover(brand5) lh(1.5) ff(proxima) fw(600) c(white) ou(0) t(all)'
+        return 'b(0) br(2px) px(1.5rem) py(0.5rem) bg(brand) bg-hover(brand5) lh(1.5) ff(inherit) fw(600) c(white) ou(0) t(all)'
+      case 'label':
+        return 'd(block) pb(0.5rem) fw(600) fs(1.3rem) c(gray5)'
       default:
         return ''
     }
   }
   
 }
+
+
